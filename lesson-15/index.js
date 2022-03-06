b85de12d50018f2bddc0364327f791c35d838e04
kost321 <kostyannn1996@gmail.com>
2022-03-06 17:14:50 +0300

    feat: completed homework HW-16

--- lesson-15/index.js
+++ lesson-15/index.js
@@ -1,27 +1,112 @@
-"use strict"
-//let a = 1
-//let b = 2
-//let c = "белых медведей";
-//console.log(`${a}${b} ${c}`);
+"use strict";
+
+/*Task from learn.JS.ru
+
+/*let name;
+let admin=`Джон`;
+name=admin;
+alert(admin);*/
 
 
-//let a = "access";
-//let b = "marine";
-//let c = "frost";
-//let d = "reproach";
-//let e = "chopped";
-//var lengthWords=(a.length+b.length+c.length+d.length+e.length);
-//console.log(lengthWords);
+/*let name=prompt('Ваше имя?');
+alert(`Ваше имя ${name}`);*/
 
 
 
-//let name = prompt("NAME?");
-//let age = prompt('age?');
-//alert(`${name} ${age}`);
+
+/*HW-15*/
 
 
-//let a = 4;
-//let b = 3;
-//a=a-1;
-//b=b+1;
-//console.log(`${a} ${b}`);
+/*Task1
+
+let x = 20;
+let y = 58;
+let z = 42;
+console.log(x+y+z);*/
+
+
+/*Task2
+
+const secondsInMinute = 60;
+const minutesInHour = 60;
+const hoursInDay = 24;
+let daysInYear = 365;
+let myAge = 25;
+let myAgeInSeconds = (myAge*daysInYear*hoursInDay*minutesInHour*secondsInMinute);
+alert(myAgeInSeconds);*/
+
+
+/*Task 3
+
+let count = 42;
+let userName = '42';
+count = '42';
+userName = 42;
+count = Number();
+userName = String();
+console.log(typeof count);
+console.log(typeof userName);*/
+
+
+
+/*Task 4
+
+let a = 1;
+let b = 2;
+let c = "белых медведей";
+console.log(`${a}${b} ${c}`);*/
+
+
+
+/*Task5
+
+let a = "access";
+let b = "marine";
+let c = "frost";
+let d = "reproach";
+let e = "chopped";
+var lengthWords=(a.length+b.length+c.length+d.length+e.length);
+console.log(lengthWords);*/
+
+
+/*Task6 
+let a = 'доступ';
+let b = 2;
+let c = true;
+let aT = typeof(a);
+let bT = typeof(b);
+let cT = typeof(c);
+alert(`Variable: a have type: ${aT}; Variable: b have type: ${bT}; Variable: c have type: ${cT};`);*/
+
+
+
+/*Task7
+
+let name = prompt("NAME?");
+let age = prompt('age?');
+alert(`${name} ${age}`);*/
+
+
+/*ADVANCED level
+
+Task1
+
+let a = 4;
+let b = 3;
+a=a-1;
+b=b+1;
+console.log(`${a} ${b}`);*/
+
+
+/*Task2
+
+let codeWord1 = "обернись";
+let codeWord2 = "неужели";
+let codeWord3 = "огурцы";
+let codeWord4 = "липкие";
+let codeWord5 = "?!";
+var cipher =(codeWord1[1]+codeWord2[1]+codeWord3[1]+codeWord4[1]+codeWord5[1]) ;
+
+console.log(cipher);*/
+
+
