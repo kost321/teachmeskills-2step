e7aefbcf73d65e93e99bfd0f30760e3b4fe739d8
kost321 <kostyannn1996@gmail.com>
2022-03-06 13:13:46 +0300

    feat: add file index.html

--- lesson-15/index.js
+++ lesson-15/index.js
@@ -1,1 +1,27 @@
-"console.log("Hello world!")"
+"use strict"
+//let a = 1
+//let b = 2
+//let c = "белых медведей";
+//console.log(`${a}${b} ${c}`);
+
+
+//let a = "access";
+//let b = "marine";
+//let c = "frost";
+//let d = "reproach";
+//let e = "chopped";
+//var lengthWords=(a.length+b.length+c.length+d.length+e.length);
+//console.log(lengthWords);
+
+
+
+//let name = prompt("NAME?");
+//let age = prompt('age?');
+//alert(`${name} ${age}`);
+
+
+//let a = 4;
+//let b = 3;
+//a=a-1;
+//b=b+1;
+//console.log(`${a} ${b}`);
