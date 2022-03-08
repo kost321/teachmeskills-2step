bfcf7dfbbd882f639f6b06f7c8686fbf85d2f104
kost321 <kostyannn1996@gmail.com>
2022-03-08 20:30:46 +0300

    fix : bugs fix in HW-16

--- lesson-15/index.js
+++ lesson-15/index.js
@@ -3,12 +3,12 @@
 /*Task from learn.JS.ru
 
 /*let name;
-let admin=`Джон`;
-name=admin;
+const admin = `Джон`;
+name = admin;
 alert(admin);*/
 
 
-/*let name=prompt('Ваше имя?');
+/*let name = prompt('Ваше имя?');
 alert(`Ваше имя ${name}`);*/
 
 
@@ -19,31 +19,29 @@ alert(`Ваше имя ${name}`);*/
 
 /*Task1
 
-let x = 20;
-let y = 58;
-let z = 42;
-console.log(x+y+z);*/
+const x = 20;
+const y = 58;
+const z = 42;
+console.log(x + y + z);*/
 
 
 /*Task2
 
-const secondsInMinute = 60;
-const minutesInHour = 60;
-const hoursInDay = 24;
+const SECONDSINMINUTE = 60;
+const MINUTESINHOUR = 60;
+const HOURSINDAY = 24;
 let daysInYear = 365;
 let myAge = 25;
-let myAgeInSeconds = (myAge*daysInYear*hoursInDay*minutesInHour*secondsInMinute);
+let myAgeInSeconds = (myAge * daysInYear * HOURSINDAY * MINUTESINHOUR * SECONDSINMINUTE);
 alert(myAgeInSeconds);*/
 
 
 /*Task 3
 
-let count = 42;
-let userName = '42';
-count = '42';
-userName = 42;
-count = Number();
-userName = String();
+const count = 42;
+const userName = '42';
+count = Number('42');
+userName = String(42);
 console.log(typeof count);
 console.log(typeof userName);*/
 
@@ -51,62 +49,91 @@ console.log(typeof userName);*/
 
 /*Task 4
 
-let a = 1;
-let b = 2;
-let c = "белых медведей";
+const a = 1;
+const b = 2;
+const c = "белых медведей";
 console.log(`${a}${b} ${c}`);*/
 
 
 
 /*Task5
 
-let a = "access";
-let b = "marine";
-let c = "frost";
-let d = "reproach";
+const a = "access";
+const b = "marine";
+const c = "frost";
+const d = "reproach";
 let e = "chopped";
-var lengthWords=(a.length+b.length+c.length+d.length+e.length);
-console.log(lengthWords);*/
+const wordLength = (a + b + c + d + e).length;
+console.log(wordLength);*/
 
 
 /*Task6 
-let a = 'доступ';
-let b = 2;
-let c = true;
-let aT = typeof(a);
-let bT = typeof(b);
-let cT = typeof(c);
-alert(`Variable: a have type: ${aT}; Variable: b have type: ${bT}; Variable: c have type: ${cT};`);*/
+const a = 'доступ';
+const b = 2;
+const c = true;
+const aType = typeof(a);
+const bType = typeof(b);
+const cType = typeof(c);
+alert(`Variable: a have type: ${aType}; Variable: b have type: ${bType}; Variable: c have type: ${cType};`);*/
 
 
 
 /*Task7
 
-let name = prompt("NAME?");
-let age = prompt('age?');
+const name = prompt("NAME?");
+const age = prompt('age?');
 alert(`${name} ${age}`);*/
 
 
-/*ADVANCED level
+//ADVANCED level
 
-Task1
+//Task1
 
-let a = 4;
-let b = 3;
-a=a-1;
-b=b+1;
+/*const a = 4;
+const b = 3;
+a=a+b;
+b=a-b;
+a=a-b;
 console.log(`${a} ${b}`);*/
 
 
 /*Task2
 
-let codeWord1 = "обернись";
-let codeWord2 = "неужели";
-let codeWord3 = "огурцы";
-let codeWord4 = "липкие";
-let codeWord5 = "?!";
-var cipher =(codeWord1[1]+codeWord2[1]+codeWord3[1]+codeWord4[1]+codeWord5[1]) ;
-
+const codeWord1 = "обернись";
+const codeWord2 = "неужели";
+const codeWord3 = "огурцы";
+const codeWord4 = "липкие";
+const codeWord5 = "?!";
+var cipher =(codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1]) ;
 console.log(cipher);*/
 
 
+
+/*hw-2-extra
+
+TASK 1
+
+let time = prompt("Количество часов");
+let litres = (time * 0.5);
+alert(Math.trunc(litres));*/
+
+
+/*TASK 2
+
+let year = prompt("Введите год");
+let century = (year - 1) / 100 + 1;
+alert(Math.trunc(century));*/
+
+
+/*TASK 3 
+
+let year = prompt("Введите год");
+let century = (year/4);
+alert("Если целое число то год високосный");
+confirm(century);*/
+
+/*TASK 4
+
+let n = prompt("Введите число");
+let result = ("$" + n);
+alert(result);*/
