95bcdce86d74f4f2e7c6a04323ba1391538c333d
kost321 <kostyannn1996@gmail.com>
2022-03-17 22:02:55 +0300

    feat: add solution for tasks 1-6 [HW-19]

---
+++ lesson-19/index.js
@@ -0,0 +1,93 @@
+"use strict"
+
+
+//task1
+
+// const box = {
+//     color: "red",
+//     size: 30
+// }
+
+// delete box.color;
+// delete box.size;
+// console.log(box);
+
+
+//task2
+
+// const box = {
+//     color: "red",
+//     size: 30
+// }
+
+// console.log("color" in box);
+// console.log("size" in box);
+// console.log(box);
+
+
+//task3
+
+// const student = {
+//     name: 'John',
+//     age: 19,
+//     isHappy: true
+// }
+
+//     for(let key in student) {
+//         console.log(key);
+//     }
+//     for(let key in student) {
+//         console.log( student[key]);
+//     }
+
+
+//task4
+
+// const colors = {
+//     'ru pum pu ru rum': {
+//         red: 'красный',
+//         green: 'зеленый',
+//         blue: 'синий'
+//     },
+// };
+
+// console.log(colors["ru pum pu ru rum"].red);
+// console.log(colors["ru pum pu ru rum"].blue);
+   
+
+//task5
+
+// let salaries = {
+//     Andrey: 500,
+//     sveta: 413,
+//     anton: 987,
+//     andrey: 664,
+//     alexandra: 199
+// }
+
+// let sum = 0;
+
+//     for (let key in salaries) {
+//         sum += salaries[key];
+//   }
+
+// console.log(sum); 
+
+
+//task6
+
+// let interLogin = prompt("Введите логин");
+// let interPassword = prompt("Введите пароль");
+
+// let result = {
+//     login : interLogin,
+//     password : interPassword
+// } 
+// let x = confirm("Вы верно ввели логи и пароль?");
+//     if (x === true) {
+//         alert("Добро пожаловать");
+//     } else {
+//         alert("Отказано");
+//     }
+
+
