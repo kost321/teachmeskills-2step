44124ee2f5b42bf2514cfe3e2c23dcebfbf37ed0
kost321 <kostyannn1996@gmail.com>
2022-03-22 21:10:45 +0300

    feat: add solution for tasks 1-7 [HW-20]

---
+++ index.js
@@ -0,0 +1,102 @@
+"use strict"
+
+// task1
+
+// const colors = ['red', 'green', 'blue'];
+
+// console.log(colors.length);
+
+
+// task2
+
+// let animals = ['monkey', 'dog', 'cat'];
+
+// console.log(animals[animals.length - 1]);
+
+
+// task3
+
+// const numbers = [5, 43, 63, 23, 90];
+
+//1
+// numbers.length = 0;
+// console.log(numbers);
+
+//2
+// console.log(numbers.slice(0,0));
+
+
+// task4
+
+// const students = ['Polina', 'Dasha', 'Masha'];
+
+// students.pop();
+// students.push(`Borya`);
+
+
+// students.shift();
+// students.unshift(`Andrey`);
+// console.log(students);
+
+
+// task5
+
+// const cats = ['Gachito', 'Tom', 'Batman'];
+
+//     for( let i = 0; i < cats.length; i += 1){
+//         console.log(cats[i]);
+//     }
+
+    // for( let i of cats) {
+    //     console.log( i );
+    // }
+
+
+// task6
+
+// const evenNumbers = [2, 4, 6, 8, 10];
+// const oddNumbers = [1, 3, 5, 7, 9];
+
+// const num = evenNumbers.concat(oddNumbers);
+// console.log([num.indexOf(8)]);
+// console.log(num);
+
+
+// task7
+
+// const binary = [0, 0, 0, 0];
+// const binaryNew = binary.join('1');
+// console.log(binaryNew);
+
+
+
+
+
+
+// function sumInput() {
+
+//     let n = [];
+
+//     while(true) {
+
+//         let value = prompt("Введите число");
+//         if( value === "" || value === null || isFinite(value)) break;
+// c
+//         n.push(value);
+//     }
+
+//     let sum = 0;
+//     for( let n of n) {
+//         sum += n;
+    
+//     }
+   
+//     return sum;
+
+// }
+
+// console.log( sumInput() );
+
+
+
+
