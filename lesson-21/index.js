598ce3d2600d509e92cfa54b741d087718a0b50d
kost321 <kostyannn1996@gmail.com>
2022-03-27 16:31:58 +0300

    feat: add solution for tasks in class work

---
+++ lesson-21/index.js
@@ -0,0 +1,27 @@
+"use strict"
+
+ 
+// let arr = [{a: 1}, {id: 2}, {id:3}, {}].filter(object => object.id)
+// console.log(arr)
+ 
+
+
+//  let a1 = [7, 7, 8, 10, 6];
+//  let a2 = [5, 6, 5, 10, 10];
+//  let a3 = [9, 9, 10, 10, 10];
+
+// let sum1 = a1.reduce((a, b,_,Array) => a+(b/Array.length, 0));
+// let sum2 = a2.reduce((a, b,_,Array) => a+(b/Array.length, 0));
+// let sum3 = a3.reduce((a, b,_,Array) => a+(b/Array.length, 0));
+// console.log(sum1,sum2,sum3)
+
+
+// const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987];
+// // fibonacci.forEach(element => console.log(element));
+
+// function a(f) {
+//     console.log(f)
+//     return f
+// }
+// fibonacci.forEach(a())
+
